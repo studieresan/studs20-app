abf5f8c813a73e51884f078ebc3ce74b6fa4dd7c
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-09-29 15:03:42 +0200

    ADD: Front end look for login view # 1
    Need to make sure this looks good on iOS as well

--- studsapp/src/loginView/loginView.js
+++ studsapp/src/loginView/loginView.js
@@ -1,14 +1,101 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+    StyleSheet, 
+    Dimensions,
+    View, 
+    TextInput,
+    Text,
+    Image,
+    TouchableHighlight
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 class LoginView extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Hello, World!</Text>
+            <LinearGradient colors={['#011660', '#002365', '#002f68', '#08396a', '#1c436a']} style={styles.top}>
+                <Login/>
+            </LinearGradient>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        return (
+            <TouchableHighlight 
+                onPress={this.props.onPress}
+                style={styles.button}
+                underlayColor='rgba(255,255,255,0.3)'
+            >
+                <Text style={styles.buttonText}>{this.props.text}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+class Login extends React.Component {
+    render() {
+        let imageSource = '../../static/images/logo.png';
+        return (
+            <View style={styles.top}>
+                <Image source={require(imageSource)} style={styles.logo} />
+                <TextInput
+                    placeholder={'Email'}
+                    placeholderTextColor={'#ccc'}
+                    style={styles.input}
+                    returnKeyType={'next'}
+                    onSubmitEditing={() => { this.passwordInput.focus(); }}
+                    blurOnSubmit={false}
+                />
+                <TextInput
+                    placeholder={'Lösenord'}
+                    placeholderTextColor={'#ccc'}
+                    secureTextEntry={true}
+                    style={styles.input}
+                    ref={(input) => { this.passwordInput = input; }}
+                    returnKeyType={'go'}
+                    onSubmitEditing={() => { alert('Logged in!'); }}
+                />
+                <Button text={'Login'} onPress={() => { alert('Logged in!'); }}/>
             </View>
         );
     }
 }
+
+const window = Dimensions.get('window');
+const styles = StyleSheet.create({
+    top: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo: {
+        height: window.height/4,
+        resizeMode: 'contain',
+        marginBottom: 20,
+    },
+    input: {
+        width: 2*window.width/3,
+        borderWidth: 1,
+        borderColor: 'white',
+        height: 50,
+        marginVertical: 5,
+        color: 'white'
+    },
+    button: {
+        backgroundColor: 'hsla(0,0%,100%,.1)',
+        borderRadius: 100,
+        paddingVertical: 12,
+        paddingHorizontal: 50,
+        borderWidth: 3,
+        borderColor: '#fac882',
+        marginVertical: 5
+    },
+    buttonText: {
+        color: '#fac882',
+        textTransform: 'uppercase'
+    }
+});
 
 export default LoginView;
