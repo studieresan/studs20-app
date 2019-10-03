afde7d583be9e70ae8d79fc7919d762b6f6e0081
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-10-03 13:40:08 +0200

    ADD: App now stores token when logged in and retrieves it on startup #8

--- studsapp/store/global/reducer.js
+++ studsapp/store/global/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import loginReducer from 'studsapp/store/global/login/reducer';
 import eventListReducer from 'studsapp/store/global/eventList/reducer';
+import authenticationReducer from 'studsapp/store/global/authentication/reducer';
 
 export default combineReducers({
     login: loginReducer,
-    eventList: eventListReducer
+    eventList: eventListReducer,
+    authentication: authenticationReducer
 });
---
+++ studsapp/utils/storage.js
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const storeData = async (key, data) => {
+    try {
+        await AsyncStorage.setItem(key, data);
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export const retrieveData = async (key) => {
+    try {
+        const data = await AsyncStorage.getItem(key);
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
