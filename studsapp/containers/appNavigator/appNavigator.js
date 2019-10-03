600bc20a840a8cf4cc09808885d16440d2300805
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-10-03 21:37:08 +0200

    ADD: Logos to tabs

--- studsapp/containers/appNavigator/appNavigator.js
+++ studsapp/containers/appNavigator/appNavigator.js
@@ -1,15 +1,46 @@
+import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import LoginView from 'studsapp/containers/loginView/loginViewContainer';
 import EventListView from 'studsapp/containers/eventListView/eventListViewContainer';
 import AuthenticationView from 'studsapp/containers/authenticationView/authenticationViewContainer';
 import SettingsView from 'studsapp/containers/settingsView/settingsViewContainer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 //TODO: Styling of tabs
 const LoggedInNavigator = createBottomTabNavigator(
     {
-        EventList: EventListView,
+        Events: EventListView,
         Settings: SettingsView
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Events') {
+                    iconName = 'ios-calendar';
+                } else if (routeName === 'Settings') {
+                    iconName = 'ios-settings';
+                }
+
+                // You can return any component that you like here!
+                return <Icon name={iconName} size={25} color={tintColor} />;
+            }
+        }),
+        tabBarOptions: {
+            activeTintColor: '#011660',
+            inactiveTintColor: '#c4a57a',
+            tabStyle: {
+                borderRightWidth: 1,
+                borderLeftWidth: 1,
+                borderColor: '#c4a57a'
+            },
+            style: {
+                backgroundColor: '#fac882',
+            },
+            showLabel: false
+        }
     }
 );
 
