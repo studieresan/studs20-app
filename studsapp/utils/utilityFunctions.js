e9d9240754ee0b0db21cabc6e09d6df16887a4f3
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-10-13 20:56:48 +0200

    FIX: Bug with date showing the wrong month #3

--- studsapp/utils/utilityFunctions.js
+++ studsapp/utils/utilityFunctions.js
@@ -1,6 +1,6 @@
 export const getDate = (date) => {
     const year = date.getFullYear();
-    const month = date.getMonth();
+    const month = date.getMonth() + 1;
     const monthString = month < 10 ? '0' + month : month;
     const day = date.getDate();
     const dayString = day < 10 ? '0' + day : day;
